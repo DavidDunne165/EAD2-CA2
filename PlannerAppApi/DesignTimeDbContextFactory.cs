d40166ee6cd5b8510f70bcfa97b674f95804ec6f
DavidD165 <75183971+DavidD165@users.noreply.github.com>
2024-03-21 14:03:09 +0000

    Trying to initialise database

---
+++ PlannerAppApi/DesignTimeDbContextFactory.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using PlannerAppApi.Data; // Make sure this is using the correct namespace for your ApplicationDbContext
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        // Build configuration
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory()) // Make sure this directory is correct for your appsettings.json file
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        // Create options builder
+        var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        var connectionString = configuration.GetConnectionString("DefaultConnection"); // Make sure this matches your connection string name in appsettings.json
+
+        builder.UseSqlServer(connectionString);
+
+        return new ApplicationDbContext(builder.Options);
+    }
+}
